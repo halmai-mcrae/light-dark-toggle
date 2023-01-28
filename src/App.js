bc5e9a0762f117f22f7898ea8990d84ecb575de6
Halmai McRae <halmaimcrae@Halmais-MacBook-Pro.local>
2023-01-28 15:28:59 +1300

    Inital commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+const ThemeContext = React.createContext({
+  theme: 'light',
+  toggleTheme: () => {},
+})
+
+function useDarkMode() {
+  const [theme, setTheme] = useState('light')
+
+  function toggleTheme() {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
+  useEffect(() => {
+    const localTheme =
+      localStorage.getItem('theme')
+    if (localTheme) {
+      setTheme(localTheme)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem('theme', theme)
+  }, [theme])
+
+  return { theme, toggleTheme }
+}
 
 function App() {
+  const darkMode = useDarkMode()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+    <ThemeContext.Provider value={darkMode}>
+      <div
+        className={`App ${
+          darkMode.theme === 'dark'
+            ? 'dark-mode'
+            : 'light-mode'
+        }`}
+      >
+        <header className="App-header">
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo"
+          />
+          <button
+            type="button"
+            class="btn btn-outline-secondary"
+            onClick={darkMode.toggleTheme}
+          >
+            Toggle Mode
+          </button>
+
+          {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </a> */}
+        </header>
+      </div>
+    </ThemeContext.Provider>
+  )
 }
 
-export default App;
+export default App
