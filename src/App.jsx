bdd0ab096bae1b0762a9c91122b548552c4bc53d
Halmai McRae <98130939+halmai-mcrae@users.noreply.github.com>
2023-01-29 11:00:35 +1300

    Add logos

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@ import React, { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import About from './components/About'
 import Home from './components/Home'
-import logo from './logo.svg'
-import './App.css'
 import NavBar from './components/NavBar'
+import logo from './logo.svg'
+import logojs from './logojs.svg'
+import logobs from './logobs.svg'
+import './App.css'
+//Imports
 
+//Themes
 const ThemeContext = React.createContext({
   theme: 'light',
   toggleTheme: () => {},
@@ -13,7 +17,6 @@ const ThemeContext = React.createContext({
 
 function useDarkMode() {
   const [theme, setTheme] = useState('light')
-
   function toggleTheme() {
     setTheme(theme === 'light' ? 'dark' : 'light')
   }
@@ -29,7 +32,6 @@ function useDarkMode() {
   useEffect(() => {
     localStorage.setItem('theme', theme)
   }, [theme])
-
   return { theme, toggleTheme }
 }
 
@@ -39,19 +41,38 @@ function App() {
   return (
     <ThemeContext.Provider value={darkMode}>
       <div
-        className={`App ${
+        className={`app ${
           darkMode.theme === 'dark'
             ? 'dark-mode'
             : 'light-mode'
         }`}
       >
+        {/* Nav And Header*/}
         <NavBar />
-        <header className="App-header">
-          <img
-            src={logo}
-            className="App-logo"
-            alt="logo"
-          />
+        <header className="app-header">
+          <div className="row">
+          <div className="col-4">
+              <img
+                src={logojs}
+                className="js-logo"
+                alt="logojs"
+              />
+            </div>
+            <div className="col-4">
+              <img
+                src={logo}
+                className="app-logo"
+                alt="logo"
+              />
+            </div>
+            <div className="col-4">
+              <img
+                src={logobs}
+                className="bs-logo"
+                alt="logobs"
+              />
+            </div>
+          </div>
           <button
             type="button"
             className="btn btn-outline-secondary"
@@ -59,28 +80,17 @@ function App() {
           >
             Toggle Mode
           </button>
-
-          {/* <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
         </header>
-        <div>
-          <Routes>
-            <Route
-              path="/"
-              element={<Home />}
-            ></Route>
-            <Route
-              path="/about"
-              element={<About />}
-            />
-          </Routes>
-        </div>
+
+        {/* Routes */}
+        <Routes>
+          <Route path="/"
+            element={<Home />} />
+          <Route
+            path="/about"
+            element={<About />}
+          />
+        </Routes>
       </div>
     </ThemeContext.Provider>
   )
